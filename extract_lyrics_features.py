a3ca7afaccfb68ee57fcde4d798d3a476812595c
Amjubackit <110063824+Amjubackit@users.noreply.github.com>
2023-07-01 17:58:05 +0300

    add lyrics-handler

--- extract_lyrics_features.py
+++ extract_lyrics_features.py
@@ -1,112 +1,125 @@
+import json
 import os
+import re
 
 import pandas as pd
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
-import nltk
 
 # nltk.download('stopwords')
 # nltk.download('punkt')
 # nltk.download('cmudict')
 # nltk.download('vader_lexicon')
 
-from nltk.corpus import cmudict
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.corpus import words
+
+WORD_LIST = set(words.words())
+STOPWORD_LIST = set(stopwords.words('english'))
 
 
-def load_song_lyrics(text_file):
-    with open(text_file, 'r') as tf:
-        lyrics = tf.read()
+class LyricsHandler:
+    def __init__(self, lyrics_file):
+        if not self.is_file_valid(lyrics_file):
+            raise FileNotFoundError
 
-    return lyrics
+        self.lyrics_string = self.load_song_lyrics(lyrics_file)
+        self.lyrics_lines = [line for line in self.lyrics_string.split("\n") if line]
+        self.clean_lyrics = re.sub(r'[^a-zA-Z\s]', '', self.lyrics_string)
+        self.tokenized_lyrics = word_tokenize(self.clean_lyrics)
+
+    @staticmethod
+    def load_song_lyrics(text_file):
+        with open(text_file, 'r') as tf:
+            lyrics = re.sub(r'[^a-zA-Z0-9\s]', '', tf.read())
+            # Remove extra spaces
+            string = re.sub(r' +', ' ', lyrics)
+
+        return string.lower()
+
+    @staticmethod
+    def is_file_valid(file_path):
+        if os.path.exists(file_path):
+            file_size = os.stat(file_path).st_size
+            return file_size > 0
+        return False
+
+    @staticmethod
+    def is_slang_word(word):
+        return word.lower() not in WORD_LIST
+
+    @staticmethod
+    def is_stop_word(word):
+        return word.lower() in STOPWORD_LIST
+
+    def vers_chorus_count(self):
+        vers_count = 0
+        chorus_count = 0
+        for line in self.lyrics_lines:
+            first_word = line.split(" ")[0]
+            if first_word == "chorus":
+                chorus_count += 1
+            elif first_word == "verse":
+                vers_count += 1
+        return vers_count, chorus_count
+
+    def count_words_by_type(self, word_type, unique=False):
+        if word_type == "slang":
+            return self.count_special_words(self.is_slang_word, unique)
+        if word_type == "stop":
+            return self.count_special_words(self.is_stop_word, unique)
+        if word_type == "all":
+            return len(self.lyrics_string.split(" ")) if not unique else len(set(self.lyrics_string.split(" ")))
+
+    def count_special_words(self, detect_func, unique):
+        word_list = self.tokenized_lyrics if not unique else set(self.tokenized_lyrics)
+        # return [word for word in word_list if detect_func(word)]
+        return sum(1 for word in word_list if detect_func(word))
+
+    def sentiment_analysis(self):
+        sid = SentimentIntensityAnalyzer()
+        sentiment = sid.polarity_scores(self.lyrics_string)
+        return sentiment
+
+    def extract_lyrics_features(self):
+        word_count = self.count_words_by_type("all")
+        unique_words = self.count_words_by_type("all", unique=True)
+        stopword_count = self.count_words_by_type("stop")
+        slang_word_count = self.count_words_by_type("slang")
+        vers_cnt, chorus_cnt = self.vers_chorus_count()
+        sentiment = self.sentiment_analysis()
+
+        features = {
+            "lines_count": len(self.lyrics_lines),
+            "word_count": word_count,
+            "unique_words": unique_words,
+            "stopword_count": stopword_count,
+            "slang_word_count": slang_word_count,
+            "chorus_count": chorus_cnt,
+            "verse_count": vers_cnt,
+            **sentiment
+        }
+        return features
 
 
-def count_words(lyrics):
-    words = word_tokenize(lyrics)
-    return len(words)
+if __name__ == '__main__':
+    filepath = "/Users/bar-study/PycharmProjects/genreReco/song_lyrics/21-savage-glock-in-my-lap.txt"
+    lyrics_handler = LyricsHandler(filepath)
 
+    # print(lyrics_handler.lyrics_string)
+    # print(lyrics_handler.lyrics_lines)
 
-def average_word_length(lyrics):
-    words = word_tokenize(lyrics)
-    total_length = sum(len(word) for word in words)
-    return total_length / len(words) if len(words) > 0 else 0
+    # wc = lyrics_handler.vers_chorus_count()
+    # print(wc)
 
-
-def sentiment_analysis(lyrics):
-    sid = SentimentIntensityAnalyzer()
-    sentiment = sid.polarity_scores(lyrics)
-    return sentiment
-
-
-def find_rhymes_count(lyrics):
-    pronunciations = cmudict.dict()
-    words = word_tokenize(lyrics.lower())
-    unique_words = set(words)
-
-    rhymes_count = 0
-    for word in unique_words:
-        try:
-            phonemes = pronunciations[word]
-        except KeyError:
-            continue
-        rhyming_words = []
-        for rhyme_word, rhyme_phonemes in pronunciations.items():
-            if rhyme_word != word and rhyme_phonemes[-2:] == phonemes[-2:]:
-                rhyming_words.append(rhyme_word)
-        rhymes_count += len(rhyming_words)
-
-    return rhymes_count
-
-
-def extract_lyrics_info(song_lyrics):
-    lyrics_info = []
-    verse_num = 0
-    chorus_num = 0
-    stopwords_list = set(stopwords.words('english'))
-
-    lines = song_lyrics.split('\n')
-    lines_count = len(lines)
-    lyrics_text = ' '.join(lines)  # Combine all lines into a single string
-
-    word_count = count_words(lyrics_text)
-    avg_word_length = average_word_length(lyrics_text)
-    # sentiment = sentiment_analysis(lyrics_text)
-    rhymes = find_rhymes_count(song_lyrics)
-    rhymes_perc = round(rhymes / word_count, 2)
-    stopwords_count = sum(1 for word in word_tokenize(lyrics_text) if word.lower() in stopwords_list)
-
-    for line in lines:
-        line = line.strip()
-        if line.startswith("[Chorus"):
-            chorus_num += 1
-        elif line.startswith("[Verse"):
-            verse_num += 1
-
-    lyrics_info.append(
-        [lines_count, word_count, avg_word_length, rhymes, rhymes_perc, chorus_num, verse_num, stopwords_count])
-    return lyrics_info
-
-
-# Example usage
-songs_dir = "song_lyrics"
-filenames = next(os.walk(songs_dir), (None, None, []))[2]
-columns = [
-    'Line count',
-    'Word Count',
-    'Average Word Length',
-    'Rhymes',
-    'Rhymes Percentage',
-    'Chorus Number',
-    'Verse Number',
-    'Stopwords Count'
-]
-for filename in filenames:
-    file_path = os.path.join(songs_dir, filename)
-    lyrics = load_song_lyrics(file_path)
-    lyrics_info = extract_lyrics_info(lyrics)
-    df = pd.DataFrame(lyrics_info, columns=columns)
-    print(df)
+    sw = lyrics_handler.extract_lyrics_features()
+    print(json.dumps(sw, indent=2))
+    # is_slang = lyrics_handler.is_stop_word("it's")
+    # print(is_slang)
+    #
+    # is_slang = lyrics_handler.is_slang_word("its")
+    # print(is_slang)
